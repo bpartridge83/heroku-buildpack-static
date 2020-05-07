ba90c33b0bde938c33141a8377eef530f9a9e3c3
Brian Partridge <bpartridge83@gmail.com>
2020-05-06 23:09:11 -0500

    Restore mruby variables, gsub

--- scripts/config/lib/ngx_mruby/headers.rb
+++ scripts/config/lib/ngx_mruby/headers.rb
@@ -8,16 +8,16 @@ config = JSON.parse(File.read(USER_CONFIG)) if File.exist?(USER_CONFIG)
 req    = Nginx::Request.new
 uri    = req.var.uri
 
-r = Nginx::Request.new
-nonce = r.var.nonce
-nonceKey = r.var.nonceKey
+r = Nginx::Request.new.var
+nonce = r.nonce
+nonceKey = r.nonceKey
 
 if config["headers"]
   config["headers"].to_a.reverse.each do |route, header_hash|
     if Regexp.compile("^#{NginxConfigUtil.to_regex(route)}$") =~ uri
       header_hash.each do |key, value|
         # value must be a string
-        req.headers_out[key] = value.to_s
+        req.headers_out[key] = value.to_s.gsub(nonceKey, nonce)
       end
       break
     end
